f66cee499389aa7b2580c7688c5803770483680f
Amane Otsuka <a_otsuka@yumemi.co.jp>
2021-04-28 14:36:49 +0900

    Initial Commit

---
+++ Yumemi-ios-training/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherViewController.swift
+//  Yumemi-ios-training
+//
+//  Created by 大塚 周 on 2021/04/28.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
